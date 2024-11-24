234883f934d20786939116baee9d454684b32252
Tareq Imbasher <tareq@meccasoft.com>
2024-11-24 16:31:14 +0200

    Add missing files

---
+++ tests/Dumpy.ConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using Dumpy;
+
+var cars = new List<Helper.Car>();
+
+for (int i = 0; i < 10000; i++)
+{
+    cars.Add(new Helper.Car());
+}
+
+var sw = Stopwatch.StartNew();
+
+Dumper.DumpHtml(cars);
+
+sw.Stop();
+
+Console.WriteLine($"Elapsed time: {sw.ElapsedMilliseconds}ms");
+
+
+class Helper
+{
+    private static readonly Random _random = new();
+
+    static string GenerateRandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[_random.Next(s.Length)]).ToArray());
+    }
+
+    public class Car
+    {
+        public string Make { get; set; } = GenerateRandomString(10);
+        public string Model { get; set; } = GenerateRandomString(15);
+        public int Year { get; set; } = 2022;
+        public DateTime CreatedDate { get; set; } = new(2022, 1, 1);
+
+        public List<Feature> Features { get; set; } = new()
+        {
+            new(),
+            new(),
+            new(),
+            new(),
+            new()
+        };
+    }
+
+    public class Feature
+    {
+        public string Label { get; set; } = GenerateRandomString(15);
+        public string Description { get; set; } = GenerateRandomString(50);
+        public bool Included { get; set; } = true;
+    }
+}
