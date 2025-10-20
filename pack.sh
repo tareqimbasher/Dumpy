132c1c61e80e0bb79250b467b9ee285477a977eb
Tareq Imbasher <tareq@meccasoft.com>
2025-10-21 00:43:51 +0300

    Renamed Dumpy core project to Dumpy.Abstractions

--- pack.sh
+++ pack.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
 dotnet clean -c Release
-dotnet build -c Release
 
-dotnet pack src/Dumpy/Dumpy.csproj -c Release -o ./nupkgs --no-build \
+dotnet pack src/Dumpy.Abstractions/Dumpy.Abstractions.csproj -c Release -o ./nupkgs \
   -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg \
   -p:ContinuousIntegrationBuild=true
 
-dotnet pack src/Dumpy.Console/Dumpy.Console.csproj -c Release -o ./nupkgs --no-build \
+dotnet pack src/Dumpy.Console/Dumpy.Console.csproj -c Release -o ./nupkgs \
   -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg \
   -p:ContinuousIntegrationBuild=true
 
-dotnet pack src/Dumpy.Html/Dumpy.Html.csproj -c Release -o ./nupkgs --no-build \
+dotnet pack src/Dumpy.Html/Dumpy.Html.csproj -c Release -o ./nupkgs \
   -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg \
   -p:ContinuousIntegrationBuild=true
